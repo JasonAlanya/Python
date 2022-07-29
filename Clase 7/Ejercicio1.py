62b62598fc657f72258838838ec0127bcfef8122
Jason Alanya <jasonalanya853@gmail.com>
2022-07-29 16:58:00 -0500

    Tema 7 - Ejercicio 1

---
+++ Clase 7/Ejercicio1.py
@@ -0,0 +1,6 @@
+from Operaciones import *
+
+sumar(3,8)
+restar(7,5)
+multiplicar(5,7)
+dividir(8,3)
---
+++ Clase 7/Operaciones.py
@@ -0,0 +1,11 @@
+def sumar(a,b):
+    print(f"El resultado de la suma es {a+b}")
+
+def restar(a,b):
+    print(f"El resultado de la resta es {a-b}")
+
+def multiplicar(a,b):
+    print(f"El resultado de la multiplicación es {a*b}")
+
+def dividir(a,b):
+    print(f"El resultado de la división es {a/b}")
